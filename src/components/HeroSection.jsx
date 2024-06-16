5ba47d3159b787c17cf843c246be1d46af8efcef
abhishekchoudharywcs <abhishek.dev@webconsultancyservices.in>
2024-06-16 11:10:22 +0530

    changed hero section font size

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,7 +5,7 @@ const HeroSection = () => {
         data-aos="zoom-in-left"
         data-aos-duration="700"
         data-aos-anchor=".gradient--text"
-        className="font-bolder text-[3rem] md:text-[4.1rem] lg:text-[4.9rem] xl:text-[6rem] 2xl:text-[7rem] leading-[1.4] md:leading-[1.7] md:w-[26ch] my-0"
+        className="font-bolder text-[4rem] md:text-[4.5rem] lg:text-[4.9rem] xl:text-[6rem] 2xl:text-[7rem] leading-[1.4] md:leading-[1.7] md:w-[26ch] my-0"
       >
         Hello, I am{" "}
         <span className="gradient-text bg-gradient-to-r from-cyan-300 to-blue-500">
