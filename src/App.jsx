aba498e4d5235085498df6d2b7516c9d10b68888
abhishekchoudharywcs <abhishek.dev@webconsultancyservices.in>
2024-06-16 10:59:24 +0530

    added locomotive scroll

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,13 @@ import Skills from "./components/Skills";
 import Footer from "./components/Footer";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import LocomotiveScroll from "locomotive-scroll";
+const locomotiveScroll = new LocomotiveScroll({
+  lenisOptions: {
+    duration: 1.1,
+    smoothTouch: false,
+  },
+});
 
 function App() {
   useEffect(() => {
